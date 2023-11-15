c8178344967222b54fde74a0feae31e9c22a4582
damian-m-g <gonzalezdamianm@gmail.com>
2023-11-15 17:16:09 -0300

    Style fixed. Keep it this way!

--- main.c
+++ main.c
@@ -6,16 +6,18 @@
 #include "lpc17xx_timer.h"
 #include "lpc17xx_uart.h"
 
+
 /* func declarations */
-void configPRIO();
-void configPINS();
-void configADC();
-void configTMR();
-void configUART();
-void switchActiveDisplay();
+void configPRIO(void);
+void configPINS(void);
+void configADC(void);
+void configTMR(void);
+void configUART(void);
+void switchActiveDisplay(void);
 void setLED(uint8_t value);
 void setDisplayValue(uint8_t display);
 void loadSevenSegValue(uint8_t value, uint8_t display);
+
 
 /* global variables declaration */
 uint8_t tmr_inter_count = 0;
@@ -29,278 +31,268 @@ uint16_t adc_value;
 uint32_t port_0_on_vals[3] = {50823168, 50823168, 50823168};
 uint32_t port_0_off_vals[3] = {67108864, 67108864, 67108864};
 
+
 /* func definitions */
-int main()
-{
-    configPRIO();
-    configPINS();
-    configADC();
-    configUART();
-    configTMR();
+int main(void) {
+  configPRIO();
+  configPINS();
+  configADC();
+  configUART();
+  configTMR();
 
-    while (1)
-    {
-    }
+  while (1) {}
+
+  return 0;
 }
 
-void configPRIO()
-{
-    NVIC_SetPriority(UART0_IRQn, 0);
-    NVIC_SetPriority(ADC_IRQn, 1);
-    NVIC_SetPriority(TIMER0_IRQn, 2);
+
+void configPRIO(void) {
+  NVIC_SetPriority(UART0_IRQn, 0);
+  NVIC_SetPriority(ADC_IRQn, 1);
+  NVIC_SetPriority(TIMER0_IRQn, 2);
 }
 
-void configPINS()
-{
-    PINSEL_CFG_Type cfg;
-    cfg.Portnum = PINSEL_PORT_1;
-    cfg.Funcnum = PINSEL_FUNC_0;
-    cfg.Pinmode = PINSEL_PINMODE_PULLUP;
-    cfg.OpenDrain = PINSEL_PINMODE_NORMAL;
 
-    cfg.Pinnum = PINSEL_PIN_30;
+void configPINS(void) {
+  PINSEL_CFG_Type cfg;
+  cfg.Portnum = PINSEL_PORT_1;
+  cfg.Funcnum = PINSEL_FUNC_0;
+  cfg.Pinmode = PINSEL_PINMODE_PULLUP;
+  cfg.OpenDrain = PINSEL_PINMODE_NORMAL;
+
+  cfg.Pinnum = PINSEL_PIN_30;
+  PINSEL_ConfigPin(&cfg);
+
+  cfg.Portnum = PINSEL_PORT_0;
+  uint8_t gpioPins[13] = {0, 1, 6, 7, 8, 9, 15, 16, 17, 18, 24, 25, 26};
+
+  for (int i = 0; i <= 13; i++) {
+    cfg.Pinnum = gpioPins[i];
     PINSEL_ConfigPin(&cfg);
+  }
 
-    cfg.Portnum = PINSEL_PORT_0;
-    uint8_t gpioPins[13] = {0, 1, 6, 7, 8, 9, 15, 16, 17, 18, 24, 25, 26};
+  cfg.Funcnum = PINSEL_FUNC_1;
 
-    for (int i = 0; i <= 13; i++)
-    {
-        cfg.Pinnum = gpioPins[i];
-        PINSEL_ConfigPin(&cfg);
-    }
+  cfg.Pinnum = PINSEL_PIN_2;
+  PINSEL_ConfigPin(&cfg);
+  cfg.Pinnum = PINSEL_PIN_3;
+  PINSEL_ConfigPin(&cfg);
 
-    cfg.Funcnum = PINSEL_FUNC_1;
-
-    cfg.Pinnum = PINSEL_PIN_2;
-    PINSEL_ConfigPin(&cfg);
-    cfg.Pinnum = PINSEL_PIN_3;
-    PINSEL_ConfigPin(&cfg);
-
-    GPIO_SetDir(1, 1 << 30, 1);
-    GPIO_SetDir(0, 0b111000001111000001111000011, 1);
+  GPIO_SetDir(1, 1 << 30, 1);
+  GPIO_SetDir(0, 0b111000001111000001111000011, 1);
 }
 
-void configADC()
-{
-    ADC_Init(LPC_ADC, 200000);
 
-    // set pin for input
-    PINSEL_CFG_Type pin_0_23;
-    pin_0_23.Portnum = PINSEL_PORT_0;
-    pin_0_23.Pinnum = PINSEL_PIN_23;
-    pin_0_23.Funcnum = PINSEL_FUNC_1;
-    pin_0_23.Pinmode = PINSEL_PINMODE_TRISTATE;
-    pin_0_23.OpenDrain = PINSEL_PINMODE_NORMAL;
-    PINSEL_ConfigPin(&pin_0_23);
+void configADC(void) {
+  ADC_Init(LPC_ADC, 200000);
 
-    ADC_ChannelCmd(LPC_ADC, 0, ENABLE);
+  // set pin for input
+  PINSEL_CFG_Type pin_0_23;
+  pin_0_23.Portnum = PINSEL_PORT_0;
+  pin_0_23.Pinnum = PINSEL_PIN_23;
+  pin_0_23.Funcnum = PINSEL_FUNC_1;
+  pin_0_23.Pinmode = PINSEL_PINMODE_TRISTATE;
+  pin_0_23.OpenDrain = PINSEL_PINMODE_NORMAL;
+  PINSEL_ConfigPin(&pin_0_23);
 
-    NVIC_EnableIRQ(ADC_IRQn);
+  ADC_ChannelCmd(LPC_ADC, 0, ENABLE);
+
+  NVIC_EnableIRQ(ADC_IRQn);
 }
 
-void configTMR()
-{
-    TIM_TIMERCFG_Type tmrCfg;
-    TIM_MATCHCFG_Type mchCfg;
 
-    tmrCfg.PrescaleOption = TIM_PRESCALE_TICKVAL;
-    tmrCfg.PrescaleValue = 1;
+void configTMR(void) {
+  TIM_TIMERCFG_Type tmrCfg;
+  TIM_MATCHCFG_Type mchCfg;
 
-    mchCfg.MatchChannel = 0;
-    mchCfg.IntOnMatch = ENABLE;
-    mchCfg.ResetOnMatch = ENABLE;
-    mchCfg.StopOnMatch = DISABLE;
-    mchCfg.ExtMatchOutputType = TIM_EXTMATCH_NOTHING;
-    mchCfg.MatchValue = 165000;
+  tmrCfg.PrescaleOption = TIM_PRESCALE_TICKVAL;
+  tmrCfg.PrescaleValue = 1;
 
-    TIM_Init(LPC_TIM0, TIM_TIMER_MODE, &tmrCfg);
-    TIM_ConfigMatch(LPC_TIM0, &mchCfg);
-    TIM_Cmd(LPC_TIM0, ENABLE);
+  mchCfg.MatchChannel = 0;
+  mchCfg.IntOnMatch = ENABLE;
+  mchCfg.ResetOnMatch = ENABLE;
+  mchCfg.StopOnMatch = DISABLE;
+  mchCfg.ExtMatchOutputType = TIM_EXTMATCH_NOTHING;
+  mchCfg.MatchValue = 165000;
 
-    NVIC_EnableIRQ(TIMER0_IRQn);
+  TIM_Init(LPC_TIM0, TIM_TIMER_MODE, &tmrCfg);
+  TIM_ConfigMatch(LPC_TIM0, &mchCfg);
+  TIM_Cmd(LPC_TIM0, ENABLE);
+
+  NVIC_EnableIRQ(TIMER0_IRQn);
 }
 
-void configUART()
-{
-    UART_CFG_Type uartCfg;
-    UART_ConfigStructInit(&uartCfg);
-    uartCfg.Baud_rate = 300;
-    UART_Init((LPC_UART_TypeDef *)LPC_UART0, &uartCfg);
 
-    UART_FIFO_CFG_Type fifoCfg;
-    UART_FIFOConfigStructInit(&fifoCfg);
-    UART_FIFOConfig((LPC_UART_TypeDef *)LPC_UART0, &fifoCfg);
+void configUART(void) {
+  UART_CFG_Type uartCfg;
+  UART_ConfigStructInit(&uartCfg);
+  uartCfg.Baud_rate = 300;
+  UART_Init((LPC_UART_TypeDef *)LPC_UART0, &uartCfg);
 
-    UART_TxCmd((LPC_UART_TypeDef *)LPC_UART0, ENABLE);
-    UART_IntConfig((LPC_UART_TypeDef *)LPC_UART0, UART_INTCFG_RBR, ENABLE);
+  UART_FIFO_CFG_Type fifoCfg;
+  UART_FIFOConfigStructInit(&fifoCfg);
+  UART_FIFOConfig((LPC_UART_TypeDef *)LPC_UART0, &fifoCfg);
 
-    NVIC_EnableIRQ(UART0_IRQn);
+  UART_TxCmd((LPC_UART_TypeDef *)LPC_UART0, ENABLE);
+  UART_IntConfig((LPC_UART_TypeDef *)LPC_UART0, UART_INTCFG_RBR, ENABLE);
+
+  NVIC_EnableIRQ(UART0_IRQn);
 }
 
-void TIMER0_IRQHandler()
-{
-    switchActiveDisplay();
 
-    tmr_inter_count++;
+void TIMER0_IRQHandler(void) {
+  switchActiveDisplay();
 
-    if (tmr_inter_count == 64)
-    {
-        ADC_StartCmd(LPC_ADC, ADC_START_NOW);
-    }
+  tmr_inter_count++;
 
-    if (tmr_inter_count == 128)
-    {
-        uint8_t split_adc_value[2] = {(uint8_t)(adc_value), (uint8_t)(adc_value >> 8)};
-        UART_Send((LPC_UART_TypeDef *)LPC_UART0, split_adc_value, 2, BLOCKING);
-        tmr_inter_count = 0;
-    }
+  if (tmr_inter_count == 64) {
+    ADC_StartCmd(LPC_ADC, ADC_START_NOW);
+  }
 
-    TIM_ClearIntPending(LPC_TIM0, TIM_MR0_INT);
+  if (tmr_inter_count == 128) {
+    uint8_t split_adc_value[2] = {(uint8_t)(adc_value), (uint8_t)(adc_value >> 8)};
+    UART_Send((LPC_UART_TypeDef *)LPC_UART0, split_adc_value, 2, BLOCKING);
+    tmr_inter_count = 0;
+  }
+
+  TIM_ClearIntPending(LPC_TIM0, TIM_MR0_INT);
 }
 
-void ADC_IRQHandler()
-{
-    adc_value = ((ADC_GlobalGetData(LPC_ADC) >> 4) & 0xFFF);
+
+void ADC_IRQHandler(void) {
+  adc_value = ((ADC_GlobalGetData(LPC_ADC) >> 4) & 0xFFF);
 }
 
-void UART0_IRQHandler()
-{
-    uint8_t value = UART_ReceiveByte((LPC_UART_TypeDef *)LPC_UART0);
 
-    if (value == 255)
-    {
-        uart_inter_count = 0;
-        return;
-    }
+void UART0_IRQHandler(void) {
+  uint8_t value = UART_ReceiveByte((LPC_UART_TypeDef *)LPC_UART0);
 
-    switch (uart_inter_count)
-    {
+  if (value == 255) {
+    uart_inter_count = 0;
+    return;
+  }
+
+  switch (uart_inter_count) {
     case 0:
-        setLED(value);
-        break;
+      setLED(value);
+      break;
     case 1:
-        loadSevenSegValue(value, 0);
-        break;
+      loadSevenSegValue(value, 0);
+      break;
     case 2:
-        loadSevenSegValue(value, 1);
-        break;
+      loadSevenSegValue(value, 1);
+      break;
     case 3:
-        loadSevenSegValue(value, 2);
-    }
+      loadSevenSegValue(value, 2);
+  }
 
-    uart_inter_count++;
+  uart_inter_count++;
 }
 
-void switchActiveDisplay()
-{
-    switch (enabled_seven_seg)
-    {
+
+void switchActiveDisplay(void) {
+  switch(enabled_seven_seg) {
     case 3: // resets counter and executes case 0
-        enabled_seven_seg = 0;
+      enabled_seven_seg = 0;
     case 0: // enables second display
-        LPC_GPIO0->FIOCLR = (1 << 9);
-        LPC_GPIO0->FIOSET = (1 << 8);
-        LPC_GPIO0->FIOCLR = (1 << 7);
-        LPC_GPIO1->FIOCLR = (1 << 30); // enables dot
-        setDisplayValue(1);
-        break;
+      LPC_GPIO0->FIOCLR = (1 << 9);
+      LPC_GPIO0->FIOSET = (1 << 8);
+      LPC_GPIO0->FIOCLR = (1 << 7);
+      LPC_GPIO1->FIOCLR = (1 << 30); // enables dot
+      setDisplayValue(1);
+      break;
     case 1: // enables third display
-        LPC_GPIO0->FIOCLR = (1 << 9);
-        LPC_GPIO0->FIOCLR = (1 << 8);
-        LPC_GPIO0->FIOSET = (1 << 7);
-        LPC_GPIO1->FIOSET = (1 << 30); // disables dot
-        setDisplayValue(2);
-        break;
+      LPC_GPIO0->FIOCLR = (1 << 9);
+      LPC_GPIO0->FIOCLR = (1 << 8);
+      LPC_GPIO0->FIOSET = (1 << 7);
+      LPC_GPIO1->FIOSET = (1 << 30); // disables dot
+      setDisplayValue(2);
+      break;
     case 2: // enables first display
-        LPC_GPIO0->FIOSET = (1 << 9);
-        LPC_GPIO0->FIOCLR = (1 << 8);
-        LPC_GPIO0->FIOCLR = (1 << 7);
-        LPC_GPIO1->FIOSET = (1 << 30); // disables dot
-        setDisplayValue(0);
-        break;
-    default:
-        break;
-    }
+      LPC_GPIO0->FIOSET = (1 << 9);
+      LPC_GPIO0->FIOCLR = (1 << 8);
+      LPC_GPIO0->FIOCLR = (1 << 7);
+      LPC_GPIO1->FIOSET = (1 << 30); // disables dot
+      setDisplayValue(0);
+      break;
+  }
 
-    enabled_seven_seg++;
+  enabled_seven_seg++;
 }
 
-void setDisplayValue(uint8_t display)
-{
-    LPC_GPIO0->FIOCLR = port_0_on_vals[display];
-    LPC_GPIO0->FIOSET = port_0_off_vals[display];
+
+void setDisplayValue(uint8_t display) {
+  LPC_GPIO0->FIOCLR = port_0_on_vals[display];
+  LPC_GPIO0->FIOSET = port_0_off_vals[display];
 }
 
-void setLED(uint8_t value)
-{
-    switch (value)
-    {
+
+void setLED(uint8_t value) {
+  switch (value) {
     case 1:
-        LPC_GPIO0->FIOSET = (1 << 1);
-        LPC_GPIO0->FIOCLR = (1 << 0);
-        LPC_GPIO0->FIOCLR = (1 << 6);
-        break;
+      LPC_GPIO0->FIOSET = (1 << 1);
+      LPC_GPIO0->FIOCLR = (1 << 0);
+      LPC_GPIO0->FIOCLR = (1 << 6);
+      break;
     case 2:
-        LPC_GPIO0->FIOCLR = (1 << 1);
-        LPC_GPIO0->FIOSET = (1 << 0);
-        LPC_GPIO0->FIOCLR = (1 << 6);
-        break;
+      LPC_GPIO0->FIOCLR = (1 << 1);
+      LPC_GPIO0->FIOSET = (1 << 0);
+      LPC_GPIO0->FIOCLR = (1 << 6);
+      break;
     case 4:
-        LPC_GPIO0->FIOCLR = (1 << 1);
-        LPC_GPIO0->FIOCLR = (1 << 0);
-        LPC_GPIO0->FIOSET = (1 << 6);
-        break;
+      LPC_GPIO0->FIOCLR = (1 << 1);
+      LPC_GPIO0->FIOCLR = (1 << 0);
+      LPC_GPIO0->FIOSET = (1 << 6);
+      break;
     default:
-        break;
-    }
+      break;
+  }
 }
 
-void loadSevenSegValue(uint8_t value, uint8_t display) // segs enabled by low
-{
-    switch (value)
-    {
+
+// segs enabled by low
+void loadSevenSegValue(uint8_t value, uint8_t display) {
+  switch (value) {
     case 0:
-        port_0_on_vals[display] = 50823168;  // enables segs A,B,C,D,E,F
-        port_0_off_vals[display] = 67108864; // disables segs G
-        break;
+      port_0_on_vals[display] = 50823168;  // enables segs A,B,C,D,E,F
+      port_0_off_vals[display] = 67108864; // disables segs G
+      break;
     case 1:
-        port_0_on_vals[display] = 163840;     // enables segs B,C
-        port_0_off_vals[display] = 117768192; // disables segs A,D,E,F,G
-        break;
+      port_0_on_vals[display] = 163840;     // enables segs B,C
+      port_0_off_vals[display] = 117768192; // disables segs A,D,E,F,G
+      break;
     case 2:
-        port_0_on_vals[display] = 84344832;  // enables segs A,B,D,E,G
-        port_0_off_vals[display] = 33587200; // disables segs C,F
-        break;
+      port_0_on_vals[display] = 84344832;  // enables segs A,B,D,E,G
+      port_0_off_vals[display] = 33587200; // disables segs C,F
+      break;
     case 3:
-        port_0_on_vals[display] = 67600384;  // enables segs A,B,C,D,G
-        port_0_off_vals[display] = 50331648; // disables segs E,F
-        break;
+      port_0_on_vals[display] = 67600384;  // enables segs A,B,C,D,G
+      port_0_off_vals[display] = 50331648; // disables segs E,F
+      break;
     case 4:
-        port_0_on_vals[display] = 100827136; // enables segs B,C,F,G
-        port_0_off_vals[display] = 17104896; // disables segs A,D,E
-        break;
+      port_0_on_vals[display] = 100827136; // enables segs B,C,F,G
+      port_0_off_vals[display] = 17104896; // disables segs A,D,E
+      break;
     case 5:
-        port_0_on_vals[display] = 101023744; // enables segs A,C,D,F,G
-        port_0_off_vals[display] = 16908288; // disables segs B,E
-        break;
+      port_0_on_vals[display] = 101023744; // enables segs A,C,D,F,G
+      port_0_off_vals[display] = 16908288; // disables segs B,E
+      break;
     case 6:
-        port_0_on_vals[display] = 84377600;  // enables segs A,B,C,D,E,G
-        port_0_off_vals[display] = 33554432; // disables segs F
-        break;
+      port_0_on_vals[display] = 84377600;  // enables segs A,B,C,D,E,G
+      port_0_off_vals[display] = 33554432; // disables segs F
+      break;
     case 7:
-        port_0_on_vals[display] = 425984;     // enables segs A,B,C
-        port_0_off_vals[display] = 117506048; // disables segs D,E,F,G,H
-        break;
+      port_0_on_vals[display] = 425984;     // enables segs A,B,C
+      port_0_off_vals[display] = 117506048; // disables segs D,E,F,G,H
+      break;
     case 8:
-        port_0_on_vals[display] = 117932032; // enables segs A,B,C,D,E,F,G
-        port_0_off_vals[display] = 0;        // disables no segs
-        break;
-    case 9:
-        port_0_on_vals[display] = 101154816; // enables segs A,B,C,D,F,G
-        port_0_off_vals[display] = 16777216; // disables segs E
-        break;
+      port_0_on_vals[display] = 117932032; // enables segs A,B,C,D,E,F,G
+      port_0_off_vals[display] = 0;        // disables no segs
+      break;
     default:
-        break;
-    }
+      // 9
+      port_0_on_vals[display] = 101154816; // enables segs A,B,C,D,F,G
+      port_0_off_vals[display] = 16777216; // disables segs E
+      break;
+  }
 }
